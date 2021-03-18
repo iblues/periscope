416ea4752f798158a3b08bede762a3d1e9ba5a74
Blues <i@iblues.name>
2021-03-18 09:27:34 +0800

    目录和日志详情

---
+++ src/main/java/com/larfree/periscope/controller/IndexController.java
@@ -0,0 +1,8 @@
+package com.larfree.periscope.controller;
+
+/**
+ * @author Blues
+ * @date 2021/3/17 6:51 下午
+ **/
+public class IndexController {
+}
---
+++ src/main/java/com/larfree/periscope/service/Parser.java
@@ -0,0 +1,29 @@
+package com.larfree.periscope.service;
+
+import com.larfree.periscope.log.Interceptor;
+
+
+/**
+ * @author Blues
+ * @date 2021/3/17 6:52 下午
+ **/
+public class Parser {
+
+    private Interceptor interceptor;
+
+    public Parser() {
+        interceptor = new Interceptor();
+    }
+
+    public void getTraceList(){
+
+    }
+
+    public void getLog(String key){
+
+    }
+
+    public void getSql(String key){
+
+    }
+}
